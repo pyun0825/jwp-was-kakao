86df815a90f9513b3cff0d83fa7a99bb71a5003d
pyun0825 <pyun0825@naver.com>
2023-02-07 13:48:16 +0900

    refactor: add exceptions in handler package

--- src/main/java/webserver/handler/ResourceGetHandler.java
+++ src/main/java/webserver/handler/ResourceGetHandler.java
@@ -11,13 +11,17 @@ import java.net.URISyntaxException;
 public class ResourceGetHandler implements Handler{
     @Override
     public void handleRequest(HttpRequest request, DataOutputStream dos) throws IOException, URISyntaxException {
-        String[] splitPath = request.getRequestPath().split("\\.");
+        String[] splitPath = request.getRequestPath()
+                .orElseThrow(IllegalArgumentException::new)
+                .split("\\.");
         String fileType = splitPath[splitPath.length - 1];
         String filePathPrefix = "./static/";
-        String filePathPostfix = request.getRequestPath().substring(1);
+        String filePathPostfix = request.getRequestPath()
+                .orElseThrow(IllegalArgumentException::new)
+                .substring(1);
         if(fileType.equals("html") || fileType.equals("ico")) {
             filePathPrefix = "./templates";
-            filePathPostfix = request.getRequestPath();
+            filePathPostfix = request.getRequestPath().orElseThrow(IllegalArgumentException::new);
         }
         byte[] body = FileIoUtils.loadFileFromClasspath(filePathPrefix + filePathPostfix);
         HttpResponse httpResponse = HttpResponse.ok(body).setContentType("text/" + fileType).setContentLength();
--- src/main/java/webserver/handler/UserCreateGetHandler.java
+++ src/main/java/webserver/handler/UserCreateGetHandler.java
@@ -14,12 +14,15 @@ import java.net.URISyntaxException;
 public class UserCreateGetHandler implements Handler{
     @Override
     public void handleRequest(HttpRequest request, DataOutputStream dos) throws IOException, URISyntaxException {
-        RequestParameters requestParameters = RequestParameterParser.parse(request.getRequestQuery());
+        RequestParameters requestParameters = RequestParameterParser.parse(
+                request.getRequestQuery()
+                        .orElseThrow(IllegalArgumentException::new)
+        );
         DataBase.addUser(new User(
-                requestParameters.get("userId"),
-                requestParameters.get("password"),
-                requestParameters.get("name"),
-                requestParameters.get("email"))
+                requestParameters.get("userId").orElseThrow(IllegalArgumentException::new),
+                requestParameters.get("password").orElseThrow(IllegalArgumentException::new),
+                requestParameters.get("name").orElseThrow(IllegalArgumentException::new),
+                requestParameters.get("email").orElseThrow(IllegalArgumentException::new))
         );
         HttpResponse httpResponse = HttpResponse.found().setLocation("/index.html");
         httpResponse.sendResponse(dos);
